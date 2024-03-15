f561294d9c2880f927fbe73508d99008c5b8bf44
Flopticode <master.of.degeneracy@gmail.com>
2024-03-15 03:21:41 +0100

    Added banana

--- src/main/java/com/teamlos/psychosis/items/ItemRegistry.java
+++ src/main/java/com/teamlos/psychosis/items/ItemRegistry.java
@@ -2,13 +2,25 @@ package com.teamlos.psychosis.items;
 
 import com.teamlos.psychosis.PsychosisMod;
 
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ItemRegistry
 {
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, PsychosisMod.MODID);
 	
-	
+	public static final RegistryObject<Item> BANANA = ITEMS.register("banana", ()-> new Item(
+			new Item.Properties()
+			.stacksTo(64)
+			.food(
+					new FoodProperties.Builder()
+					.nutrition(5)
+					.saturationMod(.2f)
+					.build()
+			)
+			.rarity(Rarity.COMMON)));
 }
