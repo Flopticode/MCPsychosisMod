f53f0ece22473d7e62f2709382ccc7a45c473edd
Flopticode <master.of.degeneracy@gmail.com>
2024-03-15 03:53:14 +0100

    Added Gigabyte GPU

--- src/main/java/com/teamlos/psychosis/items/ItemRegistry.java
+++ src/main/java/com/teamlos/psychosis/items/ItemRegistry.java
@@ -3,12 +3,17 @@ package com.teamlos.psychosis.items;
 import com.teamlos.psychosis.PsychosisMod;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ItemRegistry
 {
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, PsychosisMod.MODID);
 	
-	
+	public static final RegistryObject<Item> GIGABYTE_GPU = ITEMS.register("gigabyte_gpu", ()-> new Item(
+			new Item.Properties()
+			.stacksTo(1)
+			.rarity(Rarity.COMMON)));
 }
